32a76643b901de51cf6a0d7f186c35f6f28dcbd7
wangziyue <734112060@qq.com>
2020-05-30 13:37:03 +0800

    feat: update demo

--- src/main/java/me/topits/demo/Application.java
+++ src/main/java/me/topits/demo/Application.java
@@ -1,12 +1,18 @@
 package me.topits.demo;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author WangZiyue
  */
-@SpringBootApplication
+@MapperScan({"me.itsme.**.mapper", "me.itsme.**.dao"})
+@SpringBootApplication(exclude= {DruidDataSourceAutoConfigure.class})
 public class Application {
 
     public static void main(String[] args) {
